2aa4ba38cf55999db8ed0eaa2341294eabfb104e
Marcelo <marcelo.lamas@crowdaronline.com>
2023-11-29 00:18:12 -0300

    feat

--- src/main/java/api/model/TimeInterval.java
+++ src/main/java/api/model/TimeInterval.java
@@ -1,10 +1,13 @@
 package api.model;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
+
 @Getter
 @Setter
 public class TimeInterval {
-    private String start;
-    private String end;
+    private Date start;
+    private Date end;
     private String duration;
 }
